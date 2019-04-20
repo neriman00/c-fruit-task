55856abe1144b4c5df2a57ab0ba955ba1a731554
neriman00 <neriman.kerimov.2000@bk.ru>
2019-04-20 21:52:09 +0400

    c-fruit-task

---
+++ task fruit/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_fruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            fruit banan = new fruit();
+            banan.name = "banan";
+            banan.price = 5;
+            fruit kivi = new fruit();
+            kivi.name = "kivi";
+            kivi.price = 4;
+            Console.WriteLine("Bu meyvelerimiz var.{0} ve {1}", banan.name, kivi.name);
+            Console.WriteLine("meyveleri secin");
+            string input = Console.ReadLine();
+            Console.Write("Pulunuzu daxil edin :");
+
+            if (input == "banan")
+            {
+
+
+                string money = Console.ReadLine();
+                int newMoney = Convert.ToInt32(money);
+
+                if (banan.price < newMoney)
+                {
+                    Console.WriteLine("kasibsan");
+                }
+                else
+                {
+                    Console.WriteLine("Siz banan ala bilersiniz. Meslehetli afrika kivimiz var istemirsiz?");
+                }
+            }
+             if (input == "kivi")
+            {
+                string money = Console.ReadLine();
+                int newMoney = Convert.ToInt32(money);
+                if (kivi.price > newMoney)
+                {
+                    Console.WriteLine("islemiyen dislemez");
+                }
+                else
+                {
+                    Console.WriteLine("Siz kivi ala bilersiniz.Meslehetli mozambik bananimiz var istemirsiz?");
+                }
+            }
+        }
+    }
+}
---
+++ task fruit/fruit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_fruit
+{
+    class fruit
+    {
+        public string name;
+        public int price;
+        
+    }
+}
